3defe88cff45c22adb919319bd7aa171ed3ce595
Vinaypartap Singh <77821789+Vinaypartap-Singh@users.noreply.github.com>
2024-11-21 12:18:00 -0500

    Initial Commit

---
+++ Spotify Clone/Spotify_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_CloneApp.swift
+//  Spotify Clone
+//
+//  Created by Vinay Sandhu on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
